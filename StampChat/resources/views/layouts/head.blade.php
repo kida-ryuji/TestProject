a10ca81e0bf2c5df9793a0dce9eaa0d9f1f9a6a4
karin.yumita <karin.yumita@adglobe.co.jp>
2021-04-14 11:21:54 +0900

    feat header.css

--- StampChat/resources/views/layouts/head.blade.php
+++ StampChat/resources/views/layouts/head.blade.php
@@ -6,6 +6,8 @@
 <script src="{{ asset('js/app.js')}}"></script>
 <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 
+<link rel="stylesheet" href="{{ asset('css/header.css')}}">
+
 @yield('page_css')
 
 @endsection
