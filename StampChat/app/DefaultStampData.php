630f240801573b50a3b5f0dcbec6c327dfa7fd68
takma.yamaguchi <takma.yamaguchi@adglobe.co.jp>
2021-04-23 13:11:22 +0900

    feat: (再) Dスタンプカウントアップ

--- StampChat/app/DefaultStampData.php
+++ StampChat/app/DefaultStampData.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class DefaultStampData extends Model
 {
-    //
+    protected $fillable = [
+        'count', 'update_at'
+    ];
+
+        protected $guarded = [
+        'create_at'
+    ];
+
 }
